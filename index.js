98782837811ff43a44726deb215d3200c91dafdf
James Lenahan <james.k.lenahan@gmail.com>
2020-03-31 18:36:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function map(array, func){
     });
     return mappedArray;
 }
+function reduce(array, func, startingPoint){
+    let accumulator;
+    let index;
+
+    if(!!startingPoint){
+        accumulator = startingPoint;
+        index = 0;
+    } else {
+        accumulator = func(array[0],array[1]);
+        index = 2;
+    }
+    for(index; index< array.length; index++){
+        accumulator = func(accumulator, array[index])
+    }
+    return accumulator;
+}
